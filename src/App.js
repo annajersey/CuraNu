17d675b21b489f0f6948afe3d42d5a65182887b7
anna <anna.brodskaya@gmail.com>
2019-08-24 15:16:06 +0300

    Added logo block

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./components/Header/Header";
-import styles from "./assets/styles.scss";
+import "./assets/styles.scss";
 
 const App = () =>
     <main>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
-import Bell from "./bell.svg";
-import Settings from "./settings.svg";
+import "./Header.scss";
+import Bell from "./images/bell.svg";
+import Settings from "./images/settings.svg";
 
 const Header = () =>
     <header>
-        <container>
+        <div className="container topNav">
             <nav>
                 <ul>
                     <li>Groepen</li>
@@ -15,11 +16,16 @@ const Header = () =>
                     <li>Over ons</li>
                 </ul>
             </nav>
-            <div>
-                <Bell />
-                <Settings />
+            <div className="account">
+                <div className="icon"><Settings /></div>
+                <div className="icon"><Bell /><div className="notificationsCount">1</div></div>
             </div>
-        </container>
+        </div>
+        <div className="container logo">
+            <div><img src={require("./images/logo.png")}/></div>
+            <div className="logoTitle">Cura<span>Nu</span><div className="logoSubtitle">Zorg en welzijn</div></div>
+            <div className="subtitle">Intranet</div>
+        </div>
     </header>;
 
 export default Header;
