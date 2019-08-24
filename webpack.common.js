f6f023b3e034d440065c87aa6f3d4a1e2c7f6b5c
anna <anna.brodskaya@gmail.com>
2019-08-24 12:18:04 +0300

    Added fonts

--- webpack.common.js
+++ webpack.common.js
@@ -87,6 +87,16 @@ module.exports = {
                     }
                 ]
             },
+            {
+                test: /\.(woff(2)?|ttf|eot|otf)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[ext]",
+                        outputPath: "fonts/"
+                    }
+                }]
+            }
         ]
     },
     plugins: [
