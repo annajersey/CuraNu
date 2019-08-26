0d941bcace705891ed3fcc33e63b8f5444313ea8
anna <anna.brodskaya@gmail.com>
2019-08-26 14:46:46 +0300

    Added Groups widget. Fixed widget header

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -7,10 +7,11 @@ import Accordion from "./Accordion/Accordion";
 import Events from "./Widgets/Events/Events";
 import News from "./Widgets/News/News";
 import linksData from "../../mockData/links-data.json";
-import linksData2 from "../../mockData/links2-data.json";
-import linksManualData from "../../mockData/manual-links-data.json";
+import MyLinksData from "../../mockData/own-links-data.json";
+import ManualLinksData from "../../mockData/manual-links-data.json";
 import Info from "./Widgets/Info/Info";
 import Polls from "./Widgets/Polls/Polls";
+import Groups from "./Widgets/Grops/Groups";
 
 const Content = () => {
     const [rerender, setRerenderLayout] = useState(1);
@@ -23,7 +24,7 @@ const Content = () => {
             <div className="grid-sizer"></div>
             <div className="gutter-sizer"></div>
 
-            /*---- Widgets list starts ----*/
+            {/* ---- Widgets list starts ---- */}
 
             <Accordion className="grid-block"
                 firstActiveItem={1}
@@ -37,16 +38,19 @@ const Content = () => {
                 <Polls />
             </Widget>
             <Widget type="links" title="MIJN LINKS" className="grid-block">
-                <Links data={linksData2}/>
+                <Links data={MyLinksData}/>
             </Widget>
             <Widget type="info" title="KWALITEITSHANDBOEK" className="grid-block" showMore="Naar kwaliteitshandboek">
-                <Info data={linksManualData}/>
+                <Info data={ManualLinksData}/>
             </Widget>
             <Widget className="grid-block" type="events" title="EVENEMENTEN" showMore="Meer evenementen">
                 <Events />
             </Widget>
             <Widget className="grid-block" type="events" title="NIEUWS" showMore="Meer nieuws">
                 <News />
+            </Widget>
+            <Widget className="grid-block" type="groups" title="MIJN GROEPEN" showMore="Meer groepen">
+                <Groups />
             </Widget>
         </Masonry>
     </div>;
---
+++ src/components/Content/Widgets/Grops/Groups.js
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Groups.scss";
+import groupsData from "../../../../mockData/groups-data.json";
+
+const Groups = () =>
+    <ul className="groupsWidget">
+        {groupsData.links.map((item, index) =>
+            <li key={index}>
+                {item.img &&
+                <div className="linkImage"><img src={require(`./images/${item.img}.png`)} alt={item.title}/></div>}
+                <a href={item.url}>{item.title}</a>
+            </li>
+        )}
+    </ul>;
+
+Groups.propTypes = {
+    data: PropTypes.object.isRequired
+};
+export default Groups;
