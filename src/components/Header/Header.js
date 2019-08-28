ea35f0ce1e763adb74989fdf9b97fcfd925484e4
anna <anna.brodskaya@gmail.com>
2019-08-29 00:27:13 +0300

    Responsive contact form

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
                 <HamburgerButton/>
             </div>
             <nav className={cn({showMobileMenu})} >
-                <ul>
+                <ul onClick={() => setShowMobileMenu(!showMobileMenu)}>
                     <li><a href="#">Groepen</a></li>
                     <li><Link to="/contact">Contact</Link></li>
                     <li><a href="#">FAQ</a></li>
