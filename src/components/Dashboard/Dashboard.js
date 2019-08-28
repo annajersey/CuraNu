c3401c5ad820882f3c4c79bee23a5e3ddc873f3d
anna <anna.bogomiagkova@codeit.com.ua>
2019-08-28 19:37:49 +0300

    Fix blocks position for small screens

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect, Fragment} from "react";
 import Masonry from "react-masonry-component";
 import Widget from "./Widget/Widget";
 import "./Dashboard.scss";
@@ -19,7 +19,20 @@ import Blogs from "./Widgets/Blogs/Blogs";
 import AddWidgetButton from "./Widgets/AddWidgetButton/AddWidgetButton";
 
 const Dashboard = () => {
-    const [rerender, setRerenderLayout] = useState(1);
+    const setRerenderLayout = useState(1)[1];
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+    const isSmallScreen = (windowWidth < 1180); //For small screens fixes
+
     return <div className="content container">
         <Masonry options={{
             horizontalOrder: true, columnWidth: ".grid-sizer", gutter: ".gutter-sizer",
@@ -34,52 +47,56 @@ const Dashboard = () => {
             <Accordion className="grid-block"
                 firstActiveItem={1}
                 singleOpen={true}
-                setRerenderLayout={setRerenderLayout}/>
+                setRerenderLayout={setRerenderLayout} />
 
             <Widget type="links" title="Direct Naar" className="grid-block">
-                <Links data={linksData}/>
+                <Links data={linksData} />
             </Widget>
 
-            <PhoneSearch className="grid-block"/>
+            <PhoneSearch className="grid-block" />
 
             <Widget className="grid-block" type="events" title="EVENEMENTEN" showMore="Meer evenementen">
-                <Events/>
+                <Events />
             </Widget>
 
             <Widget type="blog" title="BLOGS" className="grid-block" showMore="Meer blogs">
-                <Blogs posts={BlogPostData.posts}/>
+                <Blogs posts={BlogPostData.posts} />
             </Widget>
 
             <Widget type="blog" title="MICROBLOG" className="grid-block" showMore="Toon meer">
-                <MicroBlog/>
+                <MicroBlog />
             </Widget>
 
             <Widget className="grid-block" type="events" title="NIEUWS" showMore="Meer nieuws">
-                <News/>
+                <News />
             </Widget>
 
             <Widget type="info" title="KWALITEITSHANDBOEK" className="grid-block" showMore="Naar kwaliteitshandboek">
-                <Info data={ManualLinksData}/>
+                <Info data={ManualLinksData} />
             </Widget>
 
             <Widget className="grid-block" type="groups" title="MIJN GROEPEN" showMore="Meer groepen">
-                <Groups/>
+                <Groups />
             </Widget>
 
-            <div className="grid-block">
-                <AddWidgetButton />
-            </div>
+            {!isSmallScreen && <div className="grid-block"><AddWidgetButton /></div>}
 
             <Widget type="info" title="PEILINGEN" className="grid-block">
-                <Polls/>
+                <Polls />
             </Widget>
 
-            <div className="grid-block"></div>
-            <div className="grid-block"></div> {/*added to fix widgets position*/}
+            {!isSmallScreen &&
+            <Fragment>
+                <div className="grid-block"></div>
+                <div className="grid-block"></div>
+            </Fragment>}
 
             <Widget type="links" title="MIJN LINKS" className="grid-block">
-                <Links data={MyLinksData}/>
+                <Links data={MyLinksData} />
             </Widget>
+
+            {isSmallScreen && <div className="grid-block"></div>}
+            {isSmallScreen && <div className="grid-block"><AddWidgetButton /></div>}
 
         </Masonry>
     </div>;
